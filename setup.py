b84c0941f0bda8893bf110b365945a1c08812a57
JayashreeHV <jayashreehv7879@gmail.com>
2021-09-20 09:40:34 +0530

    workflow added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ PROJECT_NAME="oneNeuron_pypi"
 USER_NAME="jhv"
 
 setuptools.setup(
-    name=f"{PROJECT_NAME}-{USER_NAME",
+    name=f"{PROJECT_NAME}-{USER_NAME}",
     version="0.0.1",
     author=USER_NAME,
     author_email="author@example.com",
@@ -30,5 +30,4 @@ setuptools.setup(
         "numpy",
         "tqdm"
 
-    ]
 )
